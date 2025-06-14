d0801013ea9ec5cb30d1b6d9c9f6a921e1d498a4
Rishu <rishabhkaparwan8@gmail.com>
2025-06-14 14:14:29 +0530

    Add files via upload

---
+++ camera_lbph.py
@@ -0,0 +1,36 @@
+import cv2
+
+face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+face_recognizer.read('lbph_classifier.yml')
+camera = cv2.VideoCapture(0)
+width , height = 220,220
+font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+
+while True:
+
+    connected , image = camera.read()
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    detection = face_detector.detectMultiScale(image_gray,scaleFactor = 1.5,minSize = (30,30))
+
+    for (x,y,w,h) in detection:
+        image_face = cv2.resize(image_gray[y:y+w,x:x+w],(width,height))
+        cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+        id , confidence = face_recognizer.predict(image_face)
+        name = ''
+        if id == 1:
+            name = "Jones"
+        else:
+            name = 'Gabriel'
+
+        cv2.putText(image,name,(x,y+(w+30)),font , 2,(0,255,0))
+        cv2.putText(image,str(confidence),(x,y+(h+50)),font,1,(0,255,0))
+
+        cv2.imshow('Face',image)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
